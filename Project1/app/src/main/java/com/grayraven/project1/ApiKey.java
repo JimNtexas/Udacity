4f782a2bc68126f1c1642e2bd6f866874721edf9
Jim Howard <jhoward@affinegy.com>
2015-07-29 11:46:14 -0500

    =adding files from test project

---
+++ Project1/app/src/main/java/com/grayraven/project1/ApiKey.java
@@ -0,0 +1,8 @@
+package com.grayraven.project1;
+
+/**
+ * Created by jhoward on 7/29/2015.
+ */
+public class ApiKey {
+    public static String API_KEY = "000";
+}
---
+++ Project1/app/src/main/java/com/grayraven/project1/DetailsActivity.java
@@ -0,0 +1,31 @@
+package com.grayraven.project1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
+public class DetailsActivity extends ActionBarActivity {
+    private TextView titleTextView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_detail);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        String title = getIntent().getStringExtra("title");
+        String imageUrl = getIntent().getStringExtra("imageUrl");
+        titleTextView = (TextView) findViewById(R.id.title);
+        imageView = (ImageView) findViewById(R.id.grid_item_image);
+        titleTextView.setText(Html.fromHtml(title));
+
+        Picasso.with(this).load(imageUrl).into(imageView);
+    }
+}
