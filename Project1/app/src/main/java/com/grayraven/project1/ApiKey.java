5839ba07e8fd12c4bc258926cc546beb0ad71f36
Jim Howard <jhoward@affinegy.com>
2015-08-23 19:09:36 -0500

    updated ApiKey.java per lint suggestion

--- Project1/app/src/main/java/com/grayraven/project1/ApiKey.java
+++ Project1/app/src/main/java/com/grayraven/project1/ApiKey.java
@@ -1,7 +1,7 @@
 package com.grayraven.project1;
 
-// ignored with git update-index --assume-unchanged <file>
-// restore with git update-index --no-assume-unchanged <file>
-public class ApiKey {
-    public static String API_KEY = "000";
+// ignored with git update-index --assume-unchanged ApiKey.java
+// restore with git update-index --no-assume-unchanged ApiKey.java
+class ApiKey {
+    public static final String API_KEY = "000";
 }
